ea24c86474b7cdc59005e3d12538b0d697dd7f3a
Mercy Wacera <waceraaamercy@gmail.com>
2025-05-12 23:25:55 -0700

    Add db action

--- src/app/action.tsx
+++ src/app/action.tsx
@@ -6,7 +6,7 @@ import { db } from "@/db";
 import { redirect } from "next/navigation";
 
 export async function createAction(formData: FormData) {
-    const value = Math.floor(parseFloat(String(formData.get('value'))))
+    const value = Math.floor(parseFloat(String(formData.get('value'))) *100)
     const description = formData.get('description') as string;
 
     console.log('formData', formData)    
